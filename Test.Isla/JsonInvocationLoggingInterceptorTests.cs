ce810879affad131b9c2e36bbbec1719588f276f
Marc Ziman <marc.ziman@gmail.com>
2014-05-19 21:28:07 +0100

    removed reactive reference - no longer needed

--- Test.Isla/JsonInvocationLoggingInterceptorTests.cs
+++ Test.Isla/JsonInvocationLoggingInterceptorTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Reactive.Concurrency;
 using Castle.Core;
 using Castle.DynamicProxy;
 using Castle.MicroKernel.Registration;
