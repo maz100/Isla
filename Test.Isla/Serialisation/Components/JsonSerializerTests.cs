b4810ca1a3a720070d9f85fefff10259415ddc15
marc <marc.ziman@gmail.com>
2014-05-19 16:05:37 +0000

    updated to include nuget package restore

--- Test.Isla/Serialisation/Components/JsonSerializerTests.cs
+++ Test.Isla/Serialisation/Components/JsonSerializerTests.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using Isla.Serialisation.Components;
 using Isla;
 using Moq;
 using Castle.DynamicProxy;
 using NUnit;
 using NUnit.Framework;
 using Isla.Logging;
+using ServiceStack.Text;
+using JsonSerializer = Isla.Serialisation.Components.JsonSerializer;
 
 namespace Test.Isla.Serialisation.Components
 {
@@ -21,12 +22,25 @@ namespace Test.Isla.Serialisation.Components
 		[Test]
 		public void TestSerialise ()
 		{
-			_jsonSerializer = new JsonSerializer ();
-			TimedInvocation timedInvocation = new TimedInvocation ();
-			timedInvocation.ElapsedTime = new TimeSpan (1, 10, 25);
-			//timedInvocation.Invocation = null;
+		    using (JsConfig.With(timeSpanHandler: TimeSpanHandler.StandardFormat, dateHandler:DateHandler.ISO8601))
+		    {
+		        _jsonSerializer = new JsonSerializer();
+		        TimedInvocation timedInvocation = new TimedInvocation();
+		        timedInvocation.ElapsedTime = new TimeSpan(1, 10, 25);
+		        //timedInvocation.Invocation = null;
 
-			var json = _jsonSerializer.Serialize (timedInvocation);
+		        var json = _jsonSerializer.Serialize(timedInvocation);
+
+		        var jsonSerialiser = new ServiceStack.Text.JsonSerializer<TimeSpan>();
+
+		        string serialisedTimespan = jsonSerialiser.SerializeToString(new TimeSpan(1, 2, 3));
+
+		        var typeSerialiser = new ServiceStack.Text.TypeSerializer<TimeSpan>();
+
+		        string typeSerialiedTimespan = typeSerialiser.SerializeToString(new TimeSpan(1, 2, 3));
+
+		        string serialize = new JsonSerializer<DateTime>().SerializeToString(DateTime.Now);
+		    }
 		}
 	}
 
