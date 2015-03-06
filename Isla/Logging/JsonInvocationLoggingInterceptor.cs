c0f7df43f8537b7c692aeccc9f36a4cde323c6d7
marc <marc.ziman@gmail.com>
2015-03-06 11:16:12 +0000

    interceptor
    Moved code to before and after intercept methods in anticipation of async logging

--- Isla/Logging/JsonInvocationLoggingInterceptor.cs
+++ Isla/Logging/JsonInvocationLoggingInterceptor.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Diagnostics;
-using Castle.DynamicProxy;
+﻿using Castle.DynamicProxy;
 using Isla.Logging.Components;
 using Isla.Serialisation.Components;
+using log4net;
 using log4net.Config;
+using System;
+using System.Diagnostics;
 using L4nLogManager = log4net.LogManager;
 
 namespace Isla.Logging
@@ -14,35 +15,19 @@ namespace Isla.Logging
         public ILogManager LogManager { get; set; }
         public InvocationSerialisation InvocationSerialisation { get; set; }
 
+        private Exception exception;
+        private Stopwatch stopwatch;
+        private TimedInvocation timedInvocation;
+        private ILog logger;
+
         #region IInterceptor implementation
 
         public void Intercept(IInvocation invocation)
         {
-            if (!L4nLogManager.GetRepository().Configured)
-            {
-                XmlConfigurator.Configure();
-            }
+            BeforeProceed(invocation);
 
-            var logger = LogManager.GetLogger(invocation.InvocationTarget.GetType().Name);
-
-            if (log4net.ThreadContext.Properties["callid"] == null)
-            {
-                log4net.ThreadContext.Properties["callid"] = Guid.NewGuid();
-            }
-
-            Exception exception = null;
-            Stopwatch stopwatch = null;
-            TimedInvocation timedInvocation = null;
             try
             {
-                timedInvocation = new TimedInvocation(invocation, InvocationSerialisation);
-                var beginTimedInvocation = new BeginTimedInvocation(timedInvocation);
-                var jsonBeginTimedInvocation = JsonSerializer.Serialize(beginTimedInvocation);
-
-                logger.Debug(jsonBeginTimedInvocation);
-
-                stopwatch = Stopwatch.StartNew();
-
                 invocation.Proceed();
             }
             catch (Exception ex)
@@ -52,35 +37,63 @@ namespace Isla.Logging
             }
             finally
             {
-                stopwatch.Stop();
-
-                timedInvocation.ElapsedTime = stopwatch.Elapsed;
-                timedInvocation.ReturnValue = invocation.ReturnValue;
-
-                var endTimedInvocation = new EndTimedInvocation(timedInvocation);
-
-                var jsonTimedInvocation = JsonSerializer.Serialize(timedInvocation);
-                var jsonEndTimedInvocation = JsonSerializer.Serialize(endTimedInvocation);
-
-
-                if (exception != null)
-                {
-                    timedInvocation.ExceptionInfo = new ExceptionInfo(exception);
-                }
-
-                jsonTimedInvocation = JsonSerializer.Serialize(timedInvocation);
-
-                // Log the final result
-                if (exception != null)
-                {
-                    logger.Error(jsonTimedInvocation);
-                }
-                else
-                {
-                    logger.Info(jsonTimedInvocation);
-                    logger.Debug(jsonEndTimedInvocation);
-                }
+                AfterProceed(invocation, exception);
             }
+        }
+
+        private void AfterProceed(IInvocation invocation, Exception exception)
+        {
+            stopwatch.Stop();
+
+            timedInvocation.ElapsedTime = stopwatch.Elapsed;
+            timedInvocation.ReturnValue = invocation.ReturnValue;
+
+            var endTimedInvocation = new EndTimedInvocation(timedInvocation);
+
+            var jsonTimedInvocation = JsonSerializer.Serialize(timedInvocation);
+            var jsonEndTimedInvocation = JsonSerializer.Serialize(endTimedInvocation);
+
+
+            if (exception != null)
+            {
+                timedInvocation.ExceptionInfo = new ExceptionInfo(exception);
+            }
+
+            jsonTimedInvocation = JsonSerializer.Serialize(timedInvocation);
+
+            // Log the final result
+            if (exception != null)
+            {
+                logger.Error(jsonTimedInvocation);
+            }
+            else
+            {
+                logger.Info(jsonTimedInvocation);
+                logger.Debug(jsonEndTimedInvocation);
+            }
+        }
+
+        private void BeforeProceed(IInvocation invocation)
+        {
+            if (!L4nLogManager.GetRepository().Configured)
+            {
+                XmlConfigurator.Configure();
+            }
+
+            logger = LogManager.GetLogger(invocation.InvocationTarget.GetType().Name);
+
+            if (log4net.ThreadContext.Properties["callid"] == null)
+            {
+                log4net.ThreadContext.Properties["callid"] = Guid.NewGuid();
+            }
+
+            timedInvocation = new TimedInvocation(invocation, InvocationSerialisation);
+            var beginTimedInvocation = new BeginTimedInvocation(timedInvocation);
+            var jsonBeginTimedInvocation = JsonSerializer.Serialize(beginTimedInvocation);
+
+            logger.Debug(jsonBeginTimedInvocation);
+
+            stopwatch = Stopwatch.StartNew();
         }
 
         #endregion
